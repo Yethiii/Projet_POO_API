17949dde1876c3da0fcc68d6d8b43a76cb727eb2
LaetitiaV <laetitia.voue@gmail.com>
2025-03-05 18:20:09 +0100

    Test - TriviaAPIService

--- POO_API.Tests/UnitTest1.cs
+++ POO_API.Tests/UnitTest1.cs
@@ -1,3 +1,6 @@
+using System.Runtime.CompilerServices;
+using Projet_POO_API.Services;
+
 namespace POO_API.Tests;
 
 using Xunit;
@@ -6,8 +9,39 @@ using Projet_POO_API;
 public class TriviaAPIServiceTests
 {
     [Fact]
-    public void Test1()
+    public void GenererUrlTest()
     {
+        var connexion = new TriviaAPIService(new HttpClient());
         
+        connexion.GenererUrl("1", "easy", "multiple");
+        
+        Assert.Equal("https://opentdb.com/api.php?amount=10&category=1&difficulty=easy&type=multiple", connexion.SetupUrl);
+    }
+
+    [Fact]
+
+    public async Task GetCategoriesTest()
+    {
+        var httpClient = new HttpClient();
+        var service = new TriviaAPIService(httpClient);
+        
+        var categories = await service.GetCategoriesAsync();
+        
+        Assert.NotNull(categories);
+        Assert.NotEmpty(categories);
+        
+    }
+    
+    [Fact]
+    public async Task GetQuizTest()
+    {
+        var httpClient = new HttpClient();
+        var service = new TriviaAPIService(httpClient);
+        
+       service.GenererUrl("9", "easy", "multiple");
+       var quiz = await service.GetQuizAsync(); 
+       
+        Assert.NotNull(quiz);
+        Assert.NotEmpty(quiz);
     }
 }
