b870129531e1340f7c4ee08730fc5f7ad78c9b99
LaetitiaV <laetitia.voue@gmail.com>
2025-03-05 18:40:56 +0100

    Test - Début des test FavorisService

---
+++ POO_API.Tests/UnitTest_FavorisService.cs
@@ -0,0 +1,43 @@
+using System.Runtime.CompilerServices;
+using Projet_POO_API.Services;
+
+namespace POO_API.Tests;
+
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Moq;
+using Microsoft.JSInterop;
+using Projet_POO_API.Services;
+using Xunit;
+
+public class FavorisServiceTests
+{
+    [Fact]
+    public async Task RecupererFavorisTest()
+    {
+        var mockJsRuntime = new Mock<IJSRuntime>();  // Simule IJSRuntime
+        mockJsRuntime
+            .Setup(js => js.InvokeAsync<string>("localStorage.getItem", It.IsAny<object[]>()))
+            .ReturnsAsync("");  // Simule un localStorage vide
+        
+        //Setup(...) "Quand localStorage.getItem("Favoris") est appelé, retourne une chaîne vide"
+
+        var service = new FavorisService(mockJsRuntime.Object); //crée un FavorisService avec le faux LocalStoragee.
+        
+        var resultat = await service.RecupererFavoris();
+        
+        Assert.NotNull(resultat);
+        Assert.Empty(resultat);
+    }
+
+    //[Fact]
+
+    //public async Task AjouterFavorisTest()
+    //{}
+    
+    //[Fact]
+    //public async Task SupprimerFavorisTest()
+    //{}
+}
+     
